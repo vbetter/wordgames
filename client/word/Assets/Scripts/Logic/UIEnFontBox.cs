1056f1bddc53f3e1dc676b4ea4ed6ca4ca3f4d50
lijing <wodemer@me.com>
2017-12-01 10:24:32 +0800

    Create UIEnFontBox.cs

---
+++ client/word/Assets/Scripts/Logic/UIEnFontBox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEnFontBox : UIBaseBox {
+
+    public int UID;
+    public string ItemName = string.Empty;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
