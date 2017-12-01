1c530abe830d8da2176b3c943db8a92442d7eee3
lijing <wodemer@me.com>
2017-12-01 10:23:12 +0800

    Create UIBase.cs

---
+++ client/word/Assets/Scripts/Logic/UIBase.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBase : MonoBehaviour
+{
+
+    protected bool IsGamePause = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void UpdateUI()
+    {
+
+    }
+
+    public virtual void Hide()
+    {
+
+    }
+}
